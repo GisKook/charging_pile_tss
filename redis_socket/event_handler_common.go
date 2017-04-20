e983f854fa567bbc00ce9a15d3f6e2a78aa7b5ee
zhangkai <zhangkai.gis@163.com>
2017-04-20 08:50:42 +0800

    clear pre charge

--- redis_socket/event_handler_common.go
+++ redis_socket/event_handler_common.go
@@ -206,6 +206,7 @@ func (socket *RedisSocket) ProccessIncomingStatus(ch chan *base.TransactionDetai
 		redis_pile.ChargingCost = 0.0
 		redis_pile.ChargingCostE = 0.0
 		redis_pile.CurrentOrderNumber = ""
+		redis_pile.PreCharge = 0.0
 	} else if new_status.Status == uint32(PROTOCOL_CHARGE_PILE_STATUS_IDLE) {
 		if redis_pile.Status != uint32(PROTOCOL_CHARGE_PILE_STATUS_CHARGING_OFFLINE) {
 			redis_pile.Timestamp = new_status.Timestamp
