6c9c9b9ad585e6778a229622c302748eb126eacf
zhangkai <zhangkai.gis@163.com>
2018-01-22 14:12:29 +0800

    add limit to calc cost

--- redis_socket/event_handler_calc_cost.go
+++ redis_socket/event_handler_calc_cost.go
@@ -3,9 +3,13 @@ package redis_socket
 import (
 	"github.com/giskook/charging_pile_tss/db"
 	"log"
+	"math"
 )
 
 func CalcCost(station_id uint32, pre_cost float32, pre_cost_ele float32, last_meter_reading float32, cur_meter_reading float32, cur_time_stamp uint64) (float32, float32) {
+	if math.Abs(float64(cur_meter_reading-last_meter_reading)) > 50 { // 50 means 50 uint elec the two next meter_reading shold not be too big
+		return pre_cost, pre_cost_ele
+	}
 	unit_price, service_price := db.GetDBSocket().GetUnitPrice(station_id, cur_time_stamp)
 	log.Println("calc cost")
 	log.Printf("station id %d, pre_cost %.2f, pre_cost_ele %.2f, last_meter_reading %.2f, cur_meter_reading %.2f, unit_price %.2f, service_price %.2f", station_id, pre_cost, pre_cost_ele, last_meter_reading, cur_meter_reading, unit_price, service_price)
